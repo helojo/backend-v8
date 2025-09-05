aa7ab4f7c4dcb2a5f673aaf718414dd2ca3bb25d
zqgame <zqpublisher@zqgame.com>
2025-09-05 17:03:37 +0800

    修改

--- android.sh
+++ android.sh
@@ -5,7 +5,15 @@ NEW_WRAP=$2
 
 [ -z "$GITHUB_WORKSPACE" ] && GITHUB_WORKSPACE="$( cd "$( dirname "$0" )"/.. && pwd )"
 
-if [[ "$VERSION" == "10.6.194" || "$VERSION" == "11.8.172" || "$VERSION" == "13.6.97" ]]; then
+ver_major=$(echo "$VERSION" | cut -d. -f1)
+ver_minor=$(echo "$VERSION" | cut -d. -f2)
+if [[ $ver_major -gt 9 ]] || ([[ $ver_major -eq 9 ]] && [[ $ver_minor -ge 0 ]]); then
+    VERSION_GE_9_0=true
+else
+    VERSION_GE_9_0=false
+fi
+
+if [ "$VERSION_GE_9_0" = true ]; then
     sudo apt-get install -y \
         pkg-config \
         git \
@@ -43,7 +51,7 @@ sudo ldconfig
 cd ~
 echo "=====[ Getting Depot Tools ]====="	
 git clone -q https://chromium.googlesource.com/chromium/tools/depot_tools.git
-if [[ "$VERSION" != "10.6.194" && "$VERSION" != "11.8.172" && "$VERSION" != "13.6.97" ]]; then
+if [ "$VERSION_GE_9_0" = false ]; then
     cd depot_tools
     git reset --hard 8d16d4a
     cd ..
@@ -61,7 +69,7 @@ echo "=====[ Fetching V8 ]====="
 fetch v8
 echo "target_os = ['android']" >> .gclient
 cd ~/v8/v8
-if [[ "$VERSION" < "13.6.97" ]]; then
+if [ "$VERSION_GE_9_0" = false ]; then
 ./build/install-build-deps-android.sh
 fi
 git checkout refs/tags/$VERSION
@@ -76,9 +84,8 @@ gclient sync
 # git apply --cached $GITHUB_WORKSPACE/patches/builtins-puerts.patches
 # git checkout -- .
 
-if [ "$VERSION" == "11.8.172" ]; then 
-  node $GITHUB_WORKSPACE/node-script/do-gitpatch.js -p $GITHUB_WORKSPACE/patches/remove_uchar_include_v11.8.172.patch
-#   node $GITHUB_WORKSPACE/node-script/do-gitpatch.js -p $GITHUB_WORKSPACE/patches/enable_wee8_v11.8.172.patch
+if [ "$VERSION" == "11.6.189" ]; then 
+  node $GITHUB_WORKSPACE/node-script/do-gitpatch.js -p $GITHUB_WORKSPACE/patches/android_enable_16kb_v11.6.189.patch
 fi
 
 CXX_SETTING="use_custom_libcxx=false"
@@ -88,22 +95,38 @@ if [ "$NEW_WRAP" == "with_new_wrap" ]; then
   CXX_SETTING="use_custom_libcxx=true"
 fi
 
-echo "=====[ add ArrayBuffer_New_Without_Stl ]====="
-if [[ "$VERSION" < "13.6.97" ]]; then
-node $GITHUB_WORKSPACE/node-script/add_arraybuffer_new_without_stl.js . $VERSION $NEW_WRAP
-fi
-node $GITHUB_WORKSPACE/node-script/patchs.js . $VERSION $NEW_WRAP
+# node $GITHUB_WORKSPACE/node-script/patchs.js . $VERSION $NEW_WRAP
+
+# 公共配置
+COMMON_ARGS="
+    target_os=\"android\"
+    is_debug=false
+    v8_enable_i18n_support=false
+    v8_use_external_startup_data=false
+    is_component_build=false
+    v8_monolithic=true
+    v8_static_library=true
+    strip_debug_info=true
+    symbol_level=0
+    $CXX_SETTING
+    use_custom_libcxx_for_host=true
+    v8_enable_sandbox=false
+    android32_ndk_api_level=21
+    android64_ndk_api_level=21
+    target_os="android"
+    v8_enable_test_features=false
+    v8_enable_extras=false
+    use_lld=true
+    thin_lto_enable=true
+"
 
 echo "=====[ Building V8 arm64 ]====="
-if [ "$VERSION" == "11.8.172" ]; then 
-    gn gen out.gn/arm64.release --args="target_os=\"android\" target_cpu=\"arm64\" is_debug=false v8_enable_i18n_support=false v8_target_cpu=\"arm64\" use_goma=false v8_use_snapshot=true v8_use_external_startup_data=false v8_static_library=true strip_debug_info=true symbol_level=0 $CXX_SETTING use_custom_libcxx_for_host=true v8_enable_pointer_compression=false v8_enable_sandbox=false v8_enable_maglev=false v8_enable_webassembly=false"
-elif [ "$VERSION" == "13.6.97" ]; then
-    gn gen out.gn/arm64.release --args="target_os=\"android\" target_cpu=\"arm64\" is_debug=false v8_enable_i18n_support=false v8_target_cpu=\"arm64\" v8_use_external_startup_data=false is_component_build=false v8_monolithic=true v8_static_library=true strip_debug_info=true symbol_level=0 $CXX_SETTING use_custom_libcxx_for_host=true v8_enable_sandbox=false v8_enable_maglev=false v8_enable_webassembly=false android32_ndk_api_level=21 android64_ndk_api_level=21 v8_enable_test_features=false v8_enable_extras=false use_lld=true thin_lto_enable=true"
-elif [ "$VERSION" == "10.6.194" ]; then
-    gn gen out.gn/arm64.release --args="target_os=\"android\" target_cpu=\"arm64\" is_debug=false v8_enable_i18n_support=false v8_target_cpu=\"arm64\" use_goma=false v8_use_snapshot=true v8_use_external_startup_data=false v8_static_library=true strip_debug_info=true symbol_level=0 $CXX_SETTING use_custom_libcxx_for_host=true v8_enable_pointer_compression=false v8_enable_sandbox=false"
-else
-    gn gen out.gn/arm64.release --args="target_os=\"android\" target_cpu=\"arm64\" is_debug=false v8_enable_i18n_support=false v8_target_cpu=\"arm64\" use_goma=false v8_use_snapshot=true v8_use_external_startup_data=false v8_static_library=true strip_debug_info=true symbol_level=0 $CXX_SETTING use_custom_libcxx_for_host=true v8_enable_pointer_compression=false"
-fi
+
+gn gen out.gn/arm64.release --args="$COMMON_ARGS
+    target_cpu=\"arm64\"
+    v8_target_cpu=\"arm64\"
+    v8_enable_webassembly=false"
+
 ninja -C out.gn/arm64.release -t clean
 ninja -v -C out.gn/arm64.release v8_monolith
 
@@ -118,15 +141,10 @@ find out.gn/ -type f -name v8cc -exec cp "{}" output/v8/Bin/Android/arm64-v8a \;
 find out.gn/ -type f -name mksnapshot -exec cp "{}" output/v8/Bin/Android/arm64-v8a \;
 
 echo "=====[ Building V8 arm ]====="
-if [ "$VERSION" == "11.8.172"  ]; then 
-    gn gen out.gn/arm.release --args="target_os=\"android\" target_cpu=\"arm\" is_debug=false v8_enable_i18n_support=false v8_target_cpu=\"arm\" use_goma=false v8_use_snapshot=true v8_use_external_startup_data=false v8_static_library=true strip_debug_info=true symbol_level=0 $CXX_SETTING use_custom_libcxx_for_host=true v8_enable_sandbox=false v8_enable_maglev=false v8_enable_webassembly=false"
-elif [ "$VERSION" == "13.6.97" ]; then
-    gn gen out.gn/arm.release --args="target_os=\"android\" target_cpu=\"arm\" is_debug=false v8_enable_i18n_support=false v8_target_cpu=\"arm\" v8_use_external_startup_data=false is_component_build=false v8_monolithic=true v8_static_library=true strip_debug_info=true symbol_level=0 $CXX_SETTING use_custom_libcxx_for_host=true v8_enable_sandbox=false v8_enable_maglev=false v8_enable_webassembly=false android32_ndk_api_level=21 android64_ndk_api_level=21"
-elif [ "$VERSION" == "10.6.194" ]; then
-    gn gen out.gn/arm.release --args="target_os=\"android\" target_cpu=\"arm\" is_debug=false v8_enable_i18n_support=false v8_target_cpu=\"arm\" use_goma=false v8_use_snapshot=true v8_use_external_startup_data=false v8_static_library=true strip_debug_info=true symbol_level=0 $CXX_SETTING use_custom_libcxx_for_host=true v8_enable_sandbox=false"
-else
-    gn gen out.gn/arm.release --args="target_os=\"android\" target_cpu=\"arm\" is_debug=false v8_enable_i18n_support=false v8_target_cpu=\"arm\" use_goma=false v8_use_snapshot=true v8_use_external_startup_data=false v8_static_library=true strip_debug_info=true symbol_level=0 $CXX_SETTING use_custom_libcxx_for_host=true"
-fi
+gn gen out.gn/arm.release --args="$COMMON_ARGS
+    target_cpu=\"arm\"
+    v8_target_cpu=\"arm\"
+    v8_enable_webassembly=false"
 ninja -C out.gn/arm.release -t clean
 ninja -v -C out.gn/arm.release v8_monolith
 
@@ -142,15 +160,10 @@ find out.gn/ -type f -name v8cc -exec cp "{}" output/v8/Bin/Android/armeabi-v7a 
 find out.gn/ -type f -name mksnapshot -exec cp "{}" output/v8/Bin/Android/armeabi-v7a \;
 
 echo "=====[ Building V8 x64 ]====="
-if [ "$VERSION" == "11.8.172" ]; then 
-    gn gen out.gn/x64.release --args="target_os=\"android\" target_cpu=\"x64\" is_debug=false v8_enable_i18n_support=false v8_target_cpu=\"x64\" use_goma=false v8_use_snapshot=true v8_use_external_startup_data=false v8_static_library=true strip_debug_info=true symbol_level=0 $CXX_SETTING use_custom_libcxx_for_host=true v8_enable_pointer_compression=false v8_enable_sandbox=false v8_enable_maglev=false v8_enable_webassembly=false"
-elif [ "$VERSION" == "13.6.97" ]; then
-    gn gen out.gn/x64.release --args="target_os=\"android\" target_cpu=\"x64\" is_debug=false v8_enable_i18n_support=false v8_target_cpu=\"x64\" v8_use_external_startup_data=false is_component_build=false v8_monolithic=true v8_static_library=true strip_debug_info=true symbol_level=0 $CXX_SETTING use_custom_libcxx_for_host=true v8_enable_sandbox=false v8_enable_maglev=false v8_enable_webassembly=false android32_ndk_api_level=21 android64_ndk_api_level=21"
-elif [ "$VERSION" == "10.6.194" ]; then
-    gn gen out.gn/x64.release --args="target_os=\"android\" target_cpu=\"x64\" is_debug=false v8_enable_i18n_support=false v8_target_cpu=\"x64\" use_goma=false v8_use_snapshot=true v8_use_external_startup_data=false v8_static_library=true strip_debug_info=true symbol_level=0 $CXX_SETTING use_custom_libcxx_for_host=true v8_enable_pointer_compression=false v8_enable_sandbox=false"
-else
-    gn gen out.gn/x64.release --args="target_os=\"android\" target_cpu=\"x64\" is_debug=false v8_enable_i18n_support=false v8_target_cpu=\"x64\" use_goma=false v8_use_snapshot=true v8_use_external_startup_data=false v8_static_library=true strip_debug_info=true symbol_level=0 $CXX_SETTING use_custom_libcxx_for_host=true v8_enable_pointer_compression=false"
-fi
+gn gen out.gn/x64.release --args="$COMMON_ARGS
+    target_cpu=\"x64\"
+    v8_target_cpu=\"x64\"
+    v8_enable_webassembly=false"
 ninja -C out.gn/x64.release -t clean
 ninja -v -C out.gn/x64.release v8_monolith
 
@@ -165,14 +178,15 @@ find out.gn/ -type f -name v8cc -exec cp "{}" output/v8/Bin/Android/x86_64 \;
 find out.gn/ -type f -name mksnapshot -exec cp "{}" output/v8/Bin/Android/x86_64 \;
 
 echo "=====[ Building V8 x86 ]====="
-if [ "$VERSION" == "11.8.172"  ]; then 
-    gn gen out.gn/x86.release --args="target_os=\"android\" target_cpu=\"x86\" is_debug=false v8_enable_i18n_support=false v8_target_cpu=\"x86\" use_goma=false v8_use_snapshot=true v8_use_external_startup_data=false v8_static_library=true strip_debug_info=true symbol_level=0 $CXX_SETTING use_custom_libcxx_for_host=true v8_enable_sandbox=false v8_enable_maglev=false v8_enable_webassembly=false"
-elif [ "$VERSION" == "13.6.97" ]; then
-    gn gen out.gn/x86.release --args="target_os=\"android\" target_cpu=\"x86\" is_debug=false v8_enable_i18n_support=false v8_target_cpu=\"x86\" v8_use_external_startup_data=false is_component_build=false v8_monolithic=true v8_static_library=true strip_debug_info=true symbol_level=0 $CXX_SETTING use_custom_libcxx_for_host=true v8_enable_sandbox=false v8_enable_maglev=false v8_enable_webassembly=false android32_ndk_api_level=21 android64_ndk_api_level=21"
-elif [ "$VERSION" == "10.6.194" ]; then
-    gn gen out.gn/x86.release --args="target_os=\"android\" target_cpu=\"x86\" is_debug=false v8_enable_i18n_support=false v8_target_cpu=\"x86\" use_goma=false v8_use_snapshot=true v8_use_external_startup_data=false v8_static_library=true strip_debug_info=true symbol_level=0 $CXX_SETTING use_custom_libcxx_for_host=true v8_enable_sandbox=false"
+if [ "$VERSION" == "11.6.189"  ]; then 
+    gn gen out.gn/arm64.release --args="$COMMON_ARGS
+    target_cpu=\"arm64\"
+    v8_target_cpu=\"arm64\""
 else
-    gn gen out.gn/x86.release --args="target_os=\"android\" target_cpu=\"x86\" is_debug=false v8_enable_i18n_support=false v8_target_cpu=\"x86\" use_goma=false v8_use_snapshot=true v8_use_external_startup_data=false v8_static_library=true strip_debug_info=true symbol_level=0 $CXX_SETTING use_custom_libcxx_for_host=true"
+    gn gen out.gn/arm64.release --args="$COMMON_ARGS
+    target_cpu=\"arm64\"
+    v8_target_cpu=\"arm64\"
+    v8_enable_webassembly=false"
 fi
 ninja -C out.gn/x86.release -t clean
 ninja -v -C out.gn/x86.release v8_monolith
